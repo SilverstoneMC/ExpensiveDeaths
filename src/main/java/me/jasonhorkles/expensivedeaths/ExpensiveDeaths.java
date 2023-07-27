07e04dfc8a8283daeddf2f3aae87554b1830c825
Rubenicos <44579213+Rubenicos@users.noreply.github.com>
2023-07-27 10:35:32 -0600

    Add advanced command execution

--- src/main/java/me/jasonhorkles/expensivedeaths/ExpensiveDeaths.java
+++ src/main/java/me/jasonhorkles/expensivedeaths/ExpensiveDeaths.java
@@ -6,9 +6,14 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 public class ExpensiveDeaths extends JavaPlugin implements Listener, CommandExecutor {
     public static ExpensiveDeaths getInstance() {
@@ -16,6 +21,7 @@ public class ExpensiveDeaths extends JavaPlugin implements Listener, CommandExec
     }
 
     private Economy econ;
+    private final Map<Execution.Type, Execution> executions = new HashMap<>();
     private static ExpensiveDeaths instance;
 
     @Override
@@ -24,6 +30,7 @@ public class ExpensiveDeaths extends JavaPlugin implements Listener, CommandExec
 
         setupEconomy();
         saveDefaultConfig();
+        loadExecutions();
 
         getServer().getPluginManager().registerEvents(new DeathEvent(this), this);
         getServer().getPluginManager().registerEvents(new RespawnEvent(this), this);
@@ -32,12 +39,20 @@ public class ExpensiveDeaths extends JavaPlugin implements Listener, CommandExec
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         saveDefaultConfig();
         reloadConfig();
+        loadExecutions();
         sender.sendMessage(ChatColor.GREEN + "ExpensiveDeaths reloaded!");
         return true;
     }
 
     public Economy getEconomy() {
         return econ;
+    }
+
+    public void run(Execution.Type type, Player player, Player agent, Function<String, String> parser) {
+        final Execution execution = this.executions.get(type);
+        if (execution != null) {
+            execution.run(player, agent, parser, type.isConsole());
+        }
     }
 
     private void setupEconomy() {
@@ -47,5 +62,22 @@ public class ExpensiveDeaths extends JavaPlugin implements Listener, CommandExec
             .getRegistration(Economy.class);
         if (rsp == null) return;
         econ = rsp.getProvider();
+    }
+
+    private void loadExecutions() {
+        this.executions.clear();
+        loadExecution(Execution.Type.DEATH_CONSOLE, "console-commands-on-death");
+        loadExecution(Execution.Type.DEATH_PLAYER, "player-commands-on-death");
+        loadExecution(Execution.Type.KILL_CONSOLE, "console-commands-on-kill");
+        loadExecution(Execution.Type.KILL_PLAYER, "player-commands-on-kill");
+        loadExecution(Execution.Type.RESPAWN_CONSOLE, "console-commands-on-respawn");
+        loadExecution(Execution.Type.RESPAWN_PLAYER, "player-commands-on-respawn");
+    }
+
+    private void loadExecution(Execution.Type type, String key) {
+        final Execution execution = Execution.of(getConfig().get("bonus." + key));
+        if (execution != null) {
+            this.executions.put(type, execution);
+        }
     }
 }
