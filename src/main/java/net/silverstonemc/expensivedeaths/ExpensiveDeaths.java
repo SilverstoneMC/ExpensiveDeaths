6ea372900547a60e8d0db0cff76b60a0eabbfdeb
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2024-04-12 14:23:14 -0600

    Add update checker

--- src/main/java/net/silverstonemc/expensivedeaths/ExpensiveDeaths.java
+++ src/main/java/net/silverstonemc/expensivedeaths/ExpensiveDeaths.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,6 +37,18 @@ public class ExpensiveDeaths extends JavaPlugin implements Listener, CommandExec
 
         getServer().getPluginManager().registerEvents(new DeathEvent(this), this);
         getServer().getPluginManager().registerEvents(new RespawnEvent(this), this);
+
+        // Log version update
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                String latest = new UpdateChecker(instance).getLatestVersion();
+                String current = instance.getDescription().getVersion();
+
+                if (latest == null) return;
+                if (!current.equals(latest)) new UpdateChecker(instance).logUpdate(current, latest);
+            }
+        }.runTaskAsynchronously(this);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
---
+++ src/main/java/net/silverstonemc/expensivedeaths/UpdateChecker.java
@@ -0,0 +1,68 @@
+package net.silverstonemc.expensivedeaths;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+public class UpdateChecker implements Listener {
+    public UpdateChecker(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    private final JavaPlugin plugin;
+
+    @EventHandler(ignoreCancelled = true)
+    public void onJoin(PlayerJoinEvent event) {
+        String pluginName = plugin.getDescription().getName();
+        if (event.getPlayer().hasPermission(pluginName.toLowerCase() + ".updatenotifs"))
+            // Check for updates asynchronously
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    String latest = getLatestVersion();
+                    String current = plugin.getDescription().getVersion();
+
+                    if (latest == null) return;
+                    if (!current.equals(latest)) event.getPlayer()
+                        .sendMessage(ChatColor.YELLOW + "An update is available for " + pluginName + "! " + ChatColor.GOLD + "(" + current + " → " + latest + ")\n" + ChatColor.DARK_AQUA + "https://github.com/SilverstoneMC/" + pluginName + "/releases/latest");
+                }
+            }.runTaskAsynchronously(plugin);
+    }
+
+    public String getLatestVersion() {
+        String pluginName = plugin.getDescription().getName();
+
+        try {
+            // Send the request
+            InputStream url = new URI("https://api.github.com/repos/SilverstoneMC/" + pluginName + "/releases/latest")
+                .toURL().openStream();
+
+            // Read the response
+            JSONObject response = new JSONObject(new String(url.readAllBytes(), StandardCharsets.UTF_8));
+            url.close();
+
+            return response.getString("tag_name");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public void logUpdate(String current, String latest) {
+        String pluginName = plugin.getDescription().getName();
+
+        plugin.getLogger()
+            .warning("An update is available for " + pluginName + "! (" + current + " → " + latest + ")");
+        plugin.getLogger().warning("https://github.com/SilverstoneMC/" + pluginName + "/releases/latest");
+    }
+}
